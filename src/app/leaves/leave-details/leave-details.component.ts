dfc819523aef8f52ea5c288185354d2ea6d1613b
Ashish <ashishmalikcmp@gmail.com>
2018-12-26 20:21:22 +0530

    Leave Details api integrated and design

--- src/app/leaves/leave-details/leave-details.component.ts
+++ src/app/leaves/leave-details/leave-details.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Validators,FormBuilder,FormGroup } from '@angular/forms';
+import { Validators,FormBuilder,FormGroup, FormControl } from '@angular/forms';
 import { LeaveService } from '../leaves.service';
 
 import { Url } from '../../Url';
@@ -12,6 +12,8 @@ import { HttpClient } from '@angular/common/http';
 })
 export class LeaveDetailsComponent implements OnInit {
 id: number;
+user: FormGroup;
+
 dataSource: any;
     constructor(public form: FormBuilder, public leaveService: LeaveService,public http: HttpClient ) { 
         
@@ -22,6 +24,17 @@ dataSource: any;
     ngOnInit() {
       this.id=this.leaveService.getLeaveId();
       this.getDetails(this.id);
+      this.user = new FormGroup({
+        id: new FormControl('', [Validators.required]),
+        fromDate: new FormControl('', [Validators.required]),
+        toDate: new FormControl('', [Validators.required]),
+        reason: new FormControl('', [Validators.required]),
+        status: new FormControl('', [Validators.required]),
+        description: new FormControl('', [Validators.required]),
+        availableLeaves: new FormControl('', [Validators.required]),
+        requestTime:new FormControl('', [Validators.required]),
+      });
+    
   }
   getDetails(id:number) {
    
@@ -38,5 +51,30 @@ dataSource: any;
        
     });
 
+  }
+  accept() {
+    return new Promise((resolve, reject) => {
+      this.http.get(Url.API_URL + '/api/leave/admin/'+this.id+'/true' )
+        .subscribe((response: any) => {
+          console.log(response);
+          resolve(response);
+        
+          
+        }, reject);
+        this.getDetails(this.id);
+    });
+  }
+  reject(){
+    return new Promise((resolve, reject) => {
+      this.http.get(Url.API_URL + '/api/leave/admin/'+this.id+'/false' )
+        .subscribe((response: any) => {
+          console.log(response);
+          resolve(response);
+         
+       
+          
+        }, reject);
+        this.getDetails(this.id); 
+    });
   }
 }
