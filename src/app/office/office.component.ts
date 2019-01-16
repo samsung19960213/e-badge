72d14c65b6f85bac35a739388626bb7686e08cc6
Aaqil <aaqilashique@mindstacksolutions.com>
2019-01-16 19:32:06 +0530

    office-details department list, desingation list, shift details, holidays list designed and integrated

--- src/app/office/office.component.ts
+++ src/app/office/office.component.ts
@@ -7,6 +7,7 @@ import { Url } from '../Url';
 import { DATEPICKER_HELPERS } from '../material-widgets/datepicker/helpers.data';
 import { companyDetails } from './office.model';
 import { STEPPER_HELPERS } from '../material-widgets/stepper/helpers.data';
+import { DatePipe } from '@angular/common';
 
 
 @Component({
@@ -20,50 +21,41 @@ export class OfficeComponent implements OnInit {
   companyDetails: any;
   stepperHelpers = STEPPER_HELPERS;
   // shiftDetails= new ShiftDetails();
+  private DeptArray: Array<any> = [];
+  private DesgnArray: Array<any> = [];
+  private ShiftArray: Array<any> = [];
+  private HolidayArray: Array<any> = [];
+  private newDept: any = {};
+private newDesgn: any = {};
+private newShift: any = {};
+private newHoliday:any= {};
+favoriteSeason:number;
+start=new Date(2015,6,1);
+end= new Date(2015,8,1);
+day = this.start;
+getTot:number;
+yearDays:number;
+sat = new Array();   //Declaring array for inserting Saturdays
+sun = new Array();
 
   events: string[] = [];
   myFilter = (d: Date): boolean => {
     const day = d.getDay();
 
-    return day !== 0 && day !== 6;
-  }
-  private fieldArray: Array<any> = [];
-  private fieldArray1: Array<any> = [];
-  private fieldArray2: Array<any> = [];
-  private newAttribute: any = {};
-
-  addFieldValue() {
-      this.fieldArray.push(this.newAttribute)
-      this.newAttribute = {};
-  }
-  addFieldValue2() {
-    this.fieldArray2.push(this.newAttribute)
-    this.newAttribute = {};
-}
-
-  addFieldValue1() {
-    this.fieldArray1.push(this.newAttribute)
-    this.newAttribute = {};
-}
-  deleteFieldValue(index) {
-      this.fieldArray.splice(index, 1);
-  }
-  
-  deleteFieldValue1(index) {
-    this.fieldArray1.splice(index, 1);
-}
-
-  addEvent(type: string, event: MatDatepickerInputEvent<Date>) {
-    this.events.push(`${type}: ${event.value}`);
-  }
-  constructor(private http: HttpClient, public formBuilder: FormBuilder) {
-    this.companyDetails = new companyDetails();
+    return day !== 0;
   }
   
   ngOnInit() {
+    this.HolidayArray=[];
     this.DepartmentList();
     this.DesignationList();
     this.ShiftList();
+    this.HolidayList();
+    this.Weekends();
+    
+  
+console.log(this.sat);
+console.log(this.sun);
     this.userForm = this.formBuilder.group({
       userCompanyName: ['', [Validators.required]],
       userSuperAdmin: ['', [Validators.required]],
@@ -75,6 +67,110 @@ export class OfficeComponent implements OnInit {
     });
     
  }
+ Weekends() {
+  var d = new Date();
+  this.yearDays = this.daysInMonth(d.getFullYear());
+  
+  for(var i=1;i<=this.yearDays;i++){    //looping through days in month
+   var newDate = new Date(d.getFullYear(),d.getMonth(),i)
+   if(newDate.getDay()==0){   //if Sunday
+     this.sun.push(this.datePipe.transform(newDate, 'yyyy-MM-dd'));
+   }
+   if(newDate.getDay()==6){   //if Saturday
+       this.sat.push(this.datePipe.transform(newDate, 'yyyy-MM-dd'));
+       
+   }
+ 
+ }
+//  for(var j=0; j< this.sun.length;j++){
+//    this.newHoliday.leaveDate =this.sun[j];
+//    this.newHoliday.leaveName='Weekend';
+//    this.HolidayArray.push(this.newHoliday);
+   
+//  }
+//  this.saveHoliday();
+//  this.HolidayArray=[];
+ }
+ daysInMonth(year) {
+   var year1 =0;
+  for(let i=0; i<12; i++){
+  //  console.log(new Date(year,i, 0).getDate());
+   var month = new Date(year,i,0).getDate(); 
+  
+    year1 = year1+  month;
+   
+  }
+ 
+ return year1;
+}
+
+  addDepartmentValue() {
+      this.DeptArray.push(this.newDept)
+      console.log(this.DeptArray);
+      this.saveDepartment();
+      this.newDept = {};
+  }
+  addHoliday() {
+    this.newHoliday.leaveDate=this.datePipe.transform(this.newHoliday.leaveDate, 'yyyy-MM-dd')
+    this.HolidayArray.push(this.newHoliday);
+    console.log(this.HolidayArray);
+    this.saveHoliday();
+    this.newHoliday ={};
+   
+  }
+  saveDepartment() {
+    return new Promise((resolve, reject) => {
+      this.http.post(Url.API_URL + 'api/department/save', this.DeptArray)
+        .subscribe((response: any) => {
+          resolve(response);
+        }, reject);
+      console.log('success');
+      setTimeout(function(){
+      this.DepartmentList();
+        }, 3000);
+    });
+  }
+ saveHoliday() {
+
+  return new Promise((resolve, reject) => {
+    this.http.post(Url.API_URL + 'api/leaveDates/save', this.HolidayArray)
+      .subscribe((response: any) => {
+        resolve(response);
+      }, reject);
+    console.log('success');
+    setTimeout(function(){
+    this.HolidayList();
+      }, 3000);
+  });
+  
+ }
+  addShiftDetails() {
+    this.ShiftArray.push(this.newShift)
+    console.log(this.ShiftArray);
+    this.newShift = {};
+}
+
+  addDesignationValue() {
+    this.DesgnArray.push(this.newDesgn)
+    console.log(this.DesgnArray);
+    this.newDesgn = {};
+ }
+//   deleteFieldValue(index) {
+//       this.fieldArray.splice(index, 1);
+//   }
+  
+//   deleteFieldValue1(index) {
+//     this.fieldArray1.splice(index, 1);
+// }
+
+  addEvent(type: string, event: MatDatepickerInputEvent<Date>) {
+    this.events.push(`${type}: ${event.value}`);
+  }
+  constructor(private http: HttpClient, public formBuilder: FormBuilder, public datePipe: DatePipe) {
+    this.companyDetails = new companyDetails();
+  }
+  
+ 
   saveDetails(companyDetails: companyDetails) {
     console.log(companyDetails);
 
@@ -85,15 +181,17 @@ export class OfficeComponent implements OnInit {
         }, reject);
       alert('success')
     });
+    
 
   }
   DepartmentList(): Promise<any> {
+    this.DeptArray=[];
     return new Promise((resolve, reject) => {
       this.http.get(Url.API_URL + 'api/department/all')
       .subscribe((response: any) => {
         resolve(response);
-        this.fieldArray=response;
-        console.log(this.fieldArray);
+        this.DeptArray=response;
+       
       },reject);
      
     });
@@ -103,8 +201,8 @@ export class OfficeComponent implements OnInit {
       this.http.get(Url.API_URL + 'api/shift/all')
       .subscribe((response: any) => {
         resolve(response);
-        this.fieldArray2=response;
-        console.log(this.fieldArray2);
+        this.ShiftArray=response;
+       
       },reject);
      
     });
@@ -114,8 +212,20 @@ export class OfficeComponent implements OnInit {
       this.http.get(Url.API_URL + 'api/desigantion/findallDesignation')
       .subscribe((response: any) => {
         resolve(response);
-        this.fieldArray1=response;
-        console.log(this.fieldArray1);
+        this.DesgnArray=response;
+      
+      },reject);
+     
+    });
+  }
+  HolidayList(): Promise<any> {
+    this.HolidayArray =[];
+    return new Promise((resolve, reject) => {
+      this.http.get(Url.API_URL + 'api/leaveDates/all')
+      .subscribe((response: any) => {
+        resolve(response);
+        this.HolidayArray=response;
+      
       },reject);
      
     });
