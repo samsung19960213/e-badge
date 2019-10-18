8bc057b0e399ca442cd84058c2394f06a7089149
Shabir Ahmad Mir <mirshabir150@gmail.com>
2019-10-18 19:49:53 +0530

    new aws credentials added

--- src/app/Url.ts
+++ src/app/Url.ts
@@ -1,12 +1,18 @@
 export class Url {
-//Production
+    //Production
     // public static API_URL = 'http://52.66.24.172:8080/ebadge/';
-    public static API_URL = 'http://localhost:8080/ebadge/';
-    public static AWS_AccessKeyId = 'AKIAJWIORNRWW7DMGHYA';
-    public static AWS_SecretAccessKey = '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';
-    public static AWS_BucketName = 'ebadge-images';
+    // public static API_URL = 'http://localhost:8080/ebadge/';
+    // public static AWS_AccessKeyId = 'AKIAJWIORNRWW7DMGHYA';
+    // public static AWS_SecretAccessKey = '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';
+    // public static AWS_BucketName = 'ebadge-images';
+
+    //CirrusLabs Production
+    public static API_URL = 'http://ebadge-live.us-east-2.elasticbeanstalk.com/';
+    public static AWS_AccessKeyId = 'AKIASDREFNTB77LX6V2G';
+    public static AWS_SecretAccessKey = '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';
+    public static AWS_BucketName = 'ebadge-files';
 }
-    
+
 //Test
 // public static API_URL = 'http://3.18.7.123:8080/ebadge/';
 // public static AWS_AccessKeyId = 'AKIAJWIORNRWW7DMGHYA';
