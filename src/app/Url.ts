8f4ef035efbc90d3eb097039b2c97bea92f05ffb
Shabir Ahmad Mir <mirshabir150@gmail.com>
2019-12-23 14:36:54 +0530

    test env url added

--- src/app/Url.ts
+++ src/app/Url.ts
@@ -1,16 +1,16 @@
 export class Url {
-    //Production
-    public static API_URL = 'http://52.66.24.172:8080/ebadge/';
-    // public static API_URL = 'http://192.168.11.2:8080/ebadge/';
+    //Test Production
+     public static API_URL = 'http://ebadge-dev-api.ap-south-1.elasticbeanstalk.com/';
+    //public static API_URL = 'http://localhost:8080/ebadge/';
     public static AWS_AccessKeyId = 'AKIAJWIORNRWW7DMGHYA';
     public static AWS_SecretAccessKey = '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';
     public static AWS_BucketName = 'ebadge-images';
-    public static AWS_BucketRegion = 'ap-south-1';
+    public static AWS_BucketRegion = 'us-east-2';
     public static AWS_BucketName_Leaves = 'ebadge-images/leaves-doc';
     public static AWS_BucketName_WFH = 'ebadge-images/WFH-doc';
 
     //CirrusLabs Production
-    // public static API_URL = 'https://ebadge-api.mindstacksolutions.com/';
+    //public static API_URL = 'https://ebadge-api.mindstacksolutions.com/';
     // public static AWS_AccessKeyId = 'AKIASDREFNTB77LX6V2G';
     // public static AWS_SecretAccessKey = '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';
     // public static AWS_BucketName = 'ebadge-files';
